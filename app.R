359eb714a2ccd3cd7af012ac451d3282cf1f63c0
Hazim Timimi <timimih@who.int>
2025-02-07 13:07:37 +0100

    Undid previous committed changes and replaced with a sum_of_row calculation for benchmark 5 numerator. This works because newinc_rdx and the collection of (newinc_pulm_labconf_rdx, newinc_pulm_clindx_rdx, newinc_ep_rdx) are mutually exclusive.

--- app.R
+++ app.R
@@ -14,7 +14,7 @@ library(kableExtra)
 
 # selected countries
 select <- c("AO",	"AR",	"AZ",	"BD",	"BY",	"BZ",	"BO",	"BR",	"KH",	"CF",	"CL",	"CN",	"CO",	"CG",	"CR",	"CU",	"KP",	"CD",	"DO",	"EC",	"SV",	"ET",	"GA",	"GT",	"GY",	"HT",	"HN", "IN",	"ID",	"JM",	"KE",	"KG",	"LA",	"LS",	"LR",	"MX",	"MN",	"MZ",	"MM",	"NA",	"NP",	"NI",	"NG",	"PK",	"PA",	"PG",	"PY",	"PE",	"PH",	"MD","RU",	"SL","ZA",	"SR",	"TJ",	"TH",	"UG",	"UA",	"TZ",	"UY",	"UZ",	"VE",	"VN",	"ZM",	"ZW")
-
+  
 ## load notifications, TPT and strategy datasets
 ### data is as of 18 October 2024
 str <- read.csv("latest_strategy_2024-10-18.csv", na.strings = "") |>
@@ -26,16 +26,11 @@ tpt <- read.csv("latest_contacts_tpt_2024-10-18.csv", na.strings = "") |>
 notif <- read.csv("latest_notifications_2024-10-18.csv", na.strings = "") |>
   select(iso2,new_labconf:tbdeaths_vr)
 
-notif_oth <- read.csv("TB_notifications_2025-02-06.csv", na.strings = "") |>
-  filter(year == 2023) |>
-  select(iso2, rdxsurvey_newinc, rdxsurvey_newinc_rdx)
-
 bench <- read.csv("dx_benchmark_exp.csv")
 
 df <- str |>
   left_join(tpt, by = "iso2") |>
   left_join(notif, by = "iso2") |>
-  left_join(notif_oth, by = "iso2") |>
   filter(!is.na(plhiv_all_screen_data_available)|iso2 %in% select) |> 
   # Restrict to the variables needed for the benchmarks
   select(iso2, country, year,
@@ -47,7 +42,7 @@ df <- str |>
          district_cxr, district,
          district_wrd,
          phcf_wrd, phcf,
-         newinc_rdx, c_newinc,
+         newinc_rdx, newinc_pulm_labconf_rdx, newinc_pulm_clindx_rdx, newinc_ep_rdx, c_newinc,
          wrd_test_capacity, presumptive,
          m_wrd_error_rate_lte_5pct, m_wrd,
          presumptive_wrd, presumptive,
@@ -58,21 +53,15 @@ df <- str |>
          rr_fqr_bdqr_lzds, rr_fqr_bdqs_lzds, rr_fqr_bdqu_lzds,
          rr_fqr_bdqr_lzdu, rr_fqr_bdqs_lzdu,
          rr_fqr,
-         rdx_data_available,
-         rdxsurvey_newinc, rdxsurvey_newinc_rdx,
-         newinc_pulm_labconf_rdx, newinc_pulm_clindx_rdx, newinc_ep_rdx,
+         newinc_pulm_labconf_rdx, newinc_pulm_clindx_rdx,
          new_labconf, new_clindx, ret_rel_labconf, ret_rel_clindx,
          district_monitor_pos_rate,
-         m_wrd_tat_lt_48h) %>%
-  rowwise() %>%
-  mutate(newinc_rdx = ifelse(rdx_data_available==61, sum(across(rdxsurvey_newinc:rdxsurvey_newinc_rdx), na.rm = T), newinc_rdx)) %>%
-  rowwise() %>%
-  mutate(newinc_rdx = ifelse(rdx_data_available==62, sum(across(newinc_pulm_labconf_rdx:newinc_ep_rdx), na.rm = T), newinc_rdx)) %>%
-  ungroup()
+         m_wrd_tat_lt_48h)
+  
 
 year <- max(df$year)
- 
-rm(str, tpt, notif, notif_oth)
+
+rm(str, tpt, notif)
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Web interface code
@@ -423,7 +412,7 @@ output$uad_benchmarks <- renderText({
                               step_text = "",
                               benchmark_number = "5",
                               benchmark_text = "Percentage of new and relapse cases tested using a WHO-recommended rapid diagnostic test as the initial diagnostic test",
-                              numerator = benchmark_data$newinc_rdx,
+                              numerator = sum_of_row(benchmark_data |> select(newinc_rdx, newinc_pulm_labconf_rdx, newinc_pulm_clindx_rdx, newinc_ep_rdx)),
                               denominator = benchmark_data$c_newinc) |> 
     
     append_benchmark_row_html(show_as_step_start = FALSE,
